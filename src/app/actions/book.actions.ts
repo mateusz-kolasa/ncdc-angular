01a9822f39001051b69a348fcdc0bc4efdb1fe68
Mateusz Kolasa <mateusz.kolasa97@gmail.com>
2021-08-12 10:59:30 +0200

    Actions defined

---
+++ src/app/actions/book.actions.ts
@@ -0,0 +1,51 @@
+import { createAction, props } from '@ngrx/store';
+import { Book } from '../domain/book';
+ 
+export const requestAddBook = createAction(
+  '[Book] Add Book Request',
+  props<{ book: Book }>()
+);
+
+export const addBook = createAction(
+  '[Book] Add Book Success',
+  props<{ book: Book }>()
+);
+
+export const requestGetOne = createAction(
+    '[Book] Get One Book Request',
+    props<{ id: number }>()
+);
+
+export const getOne = createAction(
+  '[Book] Get One Book Success',
+  props<{ book: Book }>()
+);
+
+export const requestGetByAuthor = createAction(
+  '[Book] Get By Author Request',
+  props<{ author: String }>()
+);
+
+export const getByAuthor = createAction(
+    '[Book] Get By Author Success',
+    props<{ author: String }>()
+);
+ 
+export const requestRemoveBook = createAction(
+  '[Book] Delete Book Request',
+  props<{ id: number }>()
+);
+
+export const removeBook = createAction(
+  '[Book] Delete Book Success',
+  props<{ id: number }>()
+);
+ 
+export const requestGetAll = createAction(
+  '[Book] Get All Request'
+);
+
+export const getAll = createAction(
+  '[Book] Get All Success',
+  props<{ books: Book[] }>()
+);
